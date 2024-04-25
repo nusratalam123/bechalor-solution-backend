0e1001b0a2c71b185a9f12e7a5ff4dc6df3c476a
nusratalam <nusratalam975@gmail.com>
2024-04-25 15:51:18 +0600

    add mess and flat model

---
+++ src/model/messOrder.model.ts
@@ -0,0 +1,87 @@
+import { ObjectId } from "bson";
+
+import mongoose from "mongoose";
+import validator from "validator";
+
+const messOrderSchema = new mongoose.Schema(
+  {
+    user_name: {
+      type: String,
+      required: true,
+    },
+    user_mobileNumber: {
+      type: String,
+      required: true,
+    },
+    user_secondMobileNumber: {
+      type: String,
+      required: true,
+    },
+    user_Img: {
+      type: String,
+      required: true,
+      validate: [validator.isURL, "Please provide a valid url"],
+    },
+    profession: {
+      type: String,
+      required: true,
+    },
+    present_address: {
+      type: String,
+      required: true,
+    },
+    permanent_address: {
+      type: String,
+      required: true,
+    },
+    guardian_name: {
+      type: String,
+      required: true,
+    },
+    guardian_mobileNumber: {
+      type: String,
+      required: true,
+    },
+    Relation_with_guardian: {
+      type: String,
+      required: true,
+    },
+    Relegion: {
+      type: String,
+      required: true,
+    },
+    marital: {
+      type: String,
+      required: true,
+    },
+    booking_area: {
+      type: String,
+      required: true,
+    },
+    booking_date: {
+      type: String,
+      required: true,
+    },
+    user_NIDImg: {
+      type: String,
+      required: true,
+      validate: [validator.isURL, "Please provide a valid url"],
+    },
+    user_InstituteImg: {
+      type: String,
+      required: true,
+      validate: [validator.isURL, "Please provide a valid url"],
+    },
+    user_dateOfBirthImg: {
+      type: String,
+      required: true,
+      validate: [validator.isURL, "Please provide a valid url"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const MessOrder = mongoose.model("MessOrder", messOrderSchema);
+export default MessOrder;
